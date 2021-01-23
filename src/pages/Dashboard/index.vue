77b87a64f5f304b832d30900f538c89463d836bf
1018715564 <1018715564@qq.com>
2021-01-23 21:00:33 +0800

    [feat]layout布局

---
+++ src/pages/Dashboard/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      this is success
+  </div>
+</template>
+<script lang='ts'>
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  setup() {
+
+    return {};
+
+  },
+});
+</script>
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,41 @@
 import { createRouter, createWebHistory } from "vue-router";
-
 const routerHistory = createWebHistory();
+import Layout from "../layouts/index.vue";
+
+export const constantRoutes = [
+  {
+    name: "login",
+    path: "/login",
+    component: () => import("../pages/Login/index.vue"),
+    hidden: true,
+  },
+  {
+    path: "/",
+    component: Layout,
+    redirect: "/index",
+    meta: {
+      title: "首页",
+      icon: "home-4-line",
+      affix: true,
+    },
+    children: [
+      {
+        path: "index",
+        name: "Index",
+        component: () => import("../pages/Dashboard/index.vue"),
+        meta: {
+          title: "首页",
+          icon: "home-4-line",
+          affix: true,
+        },
+      },
+    ],
+  },
+];
+
 const Router = createRouter({
   history: routerHistory,
-  routes: [
-    {
-      name: "login",
-      path: "/login",
-      component: () => import("../pages/Login/index.vue"),
-    },
-  ],
+  routes: constantRoutes,
 });
 
 export default Router;
